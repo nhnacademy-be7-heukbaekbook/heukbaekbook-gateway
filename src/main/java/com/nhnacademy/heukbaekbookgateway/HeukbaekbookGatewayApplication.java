25daf67460858f2c3fa78ea1e1f944c07b4947b6
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2024-10-23 16:25:30 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/heukbaekbookgateway/HeukbaekbookGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbookgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeukbaekbookGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeukbaekbookGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/heukbaekbookgateway/HeukbaekbookGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbookgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeukbaekbookGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
