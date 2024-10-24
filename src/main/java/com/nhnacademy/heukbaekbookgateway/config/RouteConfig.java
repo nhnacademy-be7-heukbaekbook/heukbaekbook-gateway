59845a5c0b90b5f8f13626912fdc2e9e21238913
unknown <201803148@o.cnu.ac.kr>
2024-10-24 16:52:50 +0900

    first

---
+++ src/main/java/com/nhnacademy/heukbaekbookgateway/config/RouteConfig.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.heukbaekbookgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+//    @Bean
+//    public RouteLocator shopRoute(RouteLocatorBuilder builder) {
+//
+//        return builder.routes()
+//                .route("", r -> r.path("")
+//                        .uri("http://"))
+//    }
+}
